7fd18ae3ed0c6bcfbe0bda89139a4074d95c2f8f
FazioNico <contact@nicolasfazio.ch>
2016-09-15 11:19:10 +0200

    st corr step4...

---
+++ dev/app/pages/home/home.js
@@ -0,0 +1,65 @@
+/**
+* @Author: Nicolas Fazio <webmaster-fazio>
+* @Date:   15-09-2016
+* @Email:  contact@nicolasfazio.ch
+* @Last modified by:   webmaster-fazio
+* @Last modified time: 15-09-2016
+*/
+
+import { UserPage } from './pages/user/user';
+
+export class HomePage {
+
+  constructor(appBody){
+    this.appBody = appBody
+    this.pageTitle = 'Hello world!';
+    this.initUI();
+  }
+
+  initUI(){
+    // remove all section before display UI
+    if(document.getElementsByTagName("section")[0]){
+      document.getElementsByTagName("section")[0].parentNode.removeChild(document.getElementsByTagName("section")[0])
+    }
+    // create page skeleton
+    let pageSkeleton = `
+      <section>
+        <h1>${this.pageTitle}</h1>
+        <form>
+          <p>
+            <label for="email">Email:</label> <input type="email" name="email" value="" placeholder="votreemail.ch"  /><br/>
+            <label for="password">Password:</label> <input type="password" name="password" value=""  /><br/>
+            <button>Login</button>
+          </p>
+        </form>
+      </section>`;
+    // add page skeleton in body
+    this.appBody.insertAdjacentHTML( 'afterbegin', pageSkeleton )
+    this.loadEventUI()
+
+  }
+
+  loadEventUI(){
+    let loginForm = document.getElementsByTagName("form")[0];
+    loginForm.addEventListener("submit",  event => this.onLogin(event), false)
+  }
+
+  onLogin(event){
+    event.preventDefault()
+    let validationInput = 0
+    let formInput = {}
+    let form = document.forms[0].elements
+    for (let i = 0; i < form.length; i++) {
+      if(form[i].value){
+        formInput[form[i].name] = form[i].value
+        validationInput++
+      }
+    }
+    console.log(formInput)
+    if(validationInput === 2){
+      console.log('load UserPage')
+      new UserPage(this.appBody,formInput);
+    }
+  }
+
+}
