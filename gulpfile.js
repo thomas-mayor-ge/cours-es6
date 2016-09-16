97055f353450a4f6ee0dd434d7429d716746c45a
FazioNico <contact@nicolasfazio.ch>
2016-09-16 09:37:33 +0200

    upd.require gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@
 * @Date:   15-09-2016
 * @Email:  contact@nicolasfazio.ch
 * @Last modified by:   webmaster-fazio
-* @Last modified time: 15-09-2016
+* @Last modified time: 16-09-2016
 */
 
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var babelify = require('babelify');
+var browserify = require("browserify");
 var transform = require('vinyl-transform');
 var source = require('vinyl-source-stream');
