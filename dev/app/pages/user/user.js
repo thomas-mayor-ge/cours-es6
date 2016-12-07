b771cfca69ac18dcd44c664e615b82a44ac0849b
FazioNico <contact@nicolasfazio.ch>
2016-12-07 22:11:20 +0100

    upd

--- dev/app/pages/user/user.js
+++ dev/app/pages/user/user.js
@@ -18,59 +18,77 @@ export class UserPage {
     this.initUI();
   }
 
-    initUI(){
-      // remove all section before display UI
-      if(document.getElementsByTagName("section")[0]){
-        document.getElementsByTagName("section")[0].parentNode.removeChild(document.getElementsByTagName("section")[0])
-      }
-      // create page skeleton
-      let pageSkeleton = `
-        <section>
-          <h1 id="time"></h1>
-          <p>${this.pageTitle} ${this.formData.email} !</p>
-        </section>
-      `;
-      // add page skeleton in body
-      this.appBody.insertAdjacentHTML( 'afterbegin', pageSkeleton )
-      this.displayTime()
-      this.getBackgroundIMG()
+  initUI(){
+    // remove all section before display UI
+    if(document.getElementsByTagName("section")[0]){
+      document.getElementsByTagName("section")[0].parentNode.removeChild(document.getElementsByTagName("section")[0])
     }
+    // create page skeleton
+    let pageSkeleton = `
+      <section>
+        <h1 id="time"></h1>
+        <p>${this.pageTitle} ${this.formData.email} !</p>
+      </section>
+    `;
+    // add page skeleton in body
+    this.appBody.insertAdjacentHTML( 'afterbegin', pageSkeleton )
+    document.getElementsByTagName("section")[0].style.opacity = 0;
+    this.displayTime()
+    this.getBackgroundIMG()
+  }
 
-    displayTime(){
-      let timeElement = document.getElementById('time')
+  displayTime(){
+    let timeElement = document.getElementById('time')
+    // some css with JS for time txt
+    timeElement.innerHTML = this.getTime(this.time)
+    timeElement.style.fontSize = '10rem';
+    timeElement.style.margin = '0rem';
+    timeElement.style.textShadow = '0px 0px 50px rgba(0, 0, 0, 0.21)';
+    // run interval
+    setInterval(()=>{
+      // asigne a new Date()
+      this.time = new Date();
+      //console.log(`${this.time.getHours()}:${this.time.getMinutes()}:${this.time.getSeconds()}`)
+      // replace innerHTML of time element
       timeElement.innerHTML = this.getTime(this.time)
-      setInterval(()=>{
-        this.time = new Date();
-        //console.log(`${this.time.getHours()}:${this.time.getMinutes()}:${this.time.getSeconds()}`)
-        timeElement.innerHTML = this.getTime(this.time)
-      },1000)
-    }
+    },1000)
+  }
 
-    getTime(time){
-      return    `
-      <time datetime="${(time.getFullYear() < 10)?'0'+time.getFullYear():time.getFullYear()}-${(time.getMonth() < 10)?'0'+time.getMonth():time.getMonth()}-${(time.getDate() < 10)?'0'+time.getDate():time.getDate()} ${(time.getHours() < 10)?'0'+time.getHours():time.getHours()}:${(time.getMinutes() < 10)?'0'+time.getMinutes():time.getMinutes()}:${(time.getSeconds() < 10)?'0'+time.getSeconds():time.getSeconds()}">
-        ${(time.getHours() < 10)?'0'+time.getHours():time.getHours()}:${(time.getMinutes() < 10)?'0'+time.getMinutes():time.getMinutes()}:${(time.getSeconds() < 10)?'0'+time.getSeconds():time.getSeconds()}
-      </time >
-      `;
-    }
+  getTime(time){
+    return    `
+    <time datetime="${(time.getFullYear() < 10)?'0'+time.getFullYear():time.getFullYear()}-${(time.getMonth() < 10)?'0'+time.getMonth():time.getMonth()}-${(time.getDate() < 10)?'0'+time.getDate():time.getDate()} ${(time.getHours() < 10)?'0'+time.getHours():time.getHours()}:${(time.getMinutes() < 10)?'0'+time.getMinutes():time.getMinutes()}:${(time.getSeconds() < 10)?'0'+time.getSeconds():time.getSeconds()}">
+      ${(time.getHours() < 10)?'0'+time.getHours():time.getHours()}:${(time.getMinutes() < 10)?'0'+time.getMinutes():time.getMinutes()}:${(time.getSeconds() < 10)?'0'+time.getSeconds():time.getSeconds()}
+    </time >
+    `;
+  }
 
-    getBackgroundIMG(){
-        let unsplash = new UnsplashService();
-        let queryService = unsplash.getRandomImg()
-        queryService.then((response)=>{
-          this.displayBackground(JSON.parse(response))
-        })
-    }
+  getBackgroundIMG(){
+      let unsplash = new UnsplashService();
+      let queryService = unsplash.getRandomImg()
+      queryService.then((response)=>{
+        this.displayBackground(JSON.parse(response))
+      })
+  }
 
-    displayBackground(data){
-      console.log('service response-> ')
-      console.log( data[0].urls.regular )
-      let pageContainer = document.getElementsByTagName("section")[0]
-      if(pageContainer){
-        pageContainer.style.height = `100%`;
-        pageContainer.style.width = `100%`;
-        pageContainer.style.background = `url(${data[0].urls.regular}) center center`;
-      }
+  displayBackground(data){
+    console.log('service response-> ')
+    console.log( data[0].urls.regular )
+    let pageContainer = document.getElementsByTagName("section")[0]
+    if(pageContainer){
+      // some css with JS for BG
+      pageContainer.style.height = `100%`;
+      pageContainer.style.width = `100%`;
+      pageContainer.style.position = `absolute`;
+      pageContainer.style.top = `0`;
+      pageContainer.style.left = `0`;
+      pageContainer.style.padding = `0px`;
+      pageContainer.style.textAlign = `center`;
+      pageContainer.style.color = `#fff`;
+      pageContainer.style.opacity = `1`;
+      pageContainer.style.background = `url(${data[0].urls.regular}) center center no-repeat`;
+      pageContainer.style.backgroundPosition = `cover`;
 
     }
+  }
+
 }
