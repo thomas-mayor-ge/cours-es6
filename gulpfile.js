c62e1b05abc1352e0c67e606ecf1c3787eac53de
FazioNico <contact@nicolasfazio.ch>
2016-12-09 12:05:55 +0100

    materialize css added

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@
 * @Date:   15-09-2016
 * @Email:  contact@nicolasfazio.ch
 * @Last modified by:   webmaster-fazio
-* @Last modified time: 16-09-2016
+* @Last modified time: 09-12-2016
 */
 
 // importer les modules NPM
@@ -20,13 +20,19 @@ var reload = browserSync.reload;
 // Config of project folders
 var config = {
     pages     : ['dev/www/*.html'], 
+    css       : ['dev/src/materialize/css/*.min.css', 'dev/src/css/*.css'],
+    fonts     : ['dev/src/materialize/fonts/**/*'],
+    jsDep     : ['dev/src/materialize/js/*.min.js'],
     desDir:    './dist' /* répértoire de destination (prod) */
 }
 
 // Default Gulp starting task
 gulp.task("run",[
   'build-js',
-  'copy-html'
+  'copy-html',
+  'copy-css',
+  'copy-fonts',
+  'copy-js-dep'
 ]);
 gulp.task('default', ['run'], function() {
     gulp.start('startServer', 'watch');
@@ -53,6 +59,25 @@ gulp.task("copy-html", function(){
         .pipe(reload({stream:true}));
 });
 
+// Task to copy CSS files
+gulp.task("copy-css", function(){
+    return gulp.src(config.css)
+        .pipe(gulp.dest(config.desDir+'/css'))
+        .pipe(reload({stream:true}));
+});
+
+// Task to copy Fonts files
+gulp.task("copy-fonts", function(){
+    return gulp.src(config.fonts)
+        .pipe(gulp.dest(config.desDir+'/fonts'))
+});
+
+// Task to copy JS Dependencies files
+gulp.task("copy-js-dep", function(){
+    return gulp.src(config.jsDep)
+        .pipe(gulp.dest(config.desDir+'/js'))
+});
+
 // Task to run local server
 gulp.task("startServer",  function() {
     browserSync.init({
@@ -66,5 +91,6 @@ gulp.task("startServer",  function() {
 gulp.task('watch', function() {
   gulp.watch('./dev/app/**/*.js', ['build-js']);     // watch js file changes
   gulp.watch('./dev/**/*.html', ['copy-html']);      // watch all html template file changes
+  gulp.watch('./dev/src/css/*.css', ['copy-css']);   // Simply uncomment exemple and set your own params (files-to-watch && task-to-run).
   //gulp.watch('PATH-OF-FILES-TO-WATCH', ['TASK-TO-RUN']);   // Simply uncomment exemple and set your own params (files-to-watch && task-to-run).
 })
