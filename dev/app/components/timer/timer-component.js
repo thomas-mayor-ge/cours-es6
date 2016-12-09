8abbba26559886130b248d7588fe811934e84085
FazioNico <contact@nicolasfazio.ch>
2016-12-09 11:28:50 +0100

    add timer components

---
+++ dev/app/components/timer/timer-component.js
@@ -0,0 +1,43 @@
+/**
+* @Author: Nicolas Fazio <webmaster-fazio>
+* @Date:   09-12-2016
+* @Email:  contact@nicolasfazio.ch
+* @Last modified by:   webmaster-fazio
+* @Last modified time: 09-12-2016
+*/
+
+export class TimerComponent {
+
+  constructor(){
+    console.log('Hello Timer components!')
+    this.time = new Date();
+    this.displayTime()
+  }
+
+  displayTime(){
+    let timeElement = document.getElementById('time')
+    if(timeElement){
+      // some css with JS for time txt
+      timeElement.innerHTML = this.getTime(this.time)
+      timeElement.style.fontSize = '10rem';
+      timeElement.style.margin = '0rem';
+      timeElement.style.textShadow = '0px 0px 50px rgba(0, 0, 0, 0.21)';
+      // run interval
+      setInterval(()=>{
+        // asigne a new Date()
+        this.time = new Date();
+        //console.log(`${this.time.getHours()}:${this.time.getMinutes()}:${this.time.getSeconds()}`)
+        // replace innerHTML of time element
+        timeElement.innerHTML = this.getTime(this.time)
+      },1000)
+    }
+  }
+
+  getTime(time){
+    return    `
+    <time datetime="${(time.getFullYear() < 10)?'0'+time.getFullYear():time.getFullYear()}-${(time.getMonth() < 10)?'0'+time.getMonth():time.getMonth()}-${(time.getDate() < 10)?'0'+time.getDate():time.getDate()} ${(time.getHours() < 10)?'0'+time.getHours():time.getHours()}:${(time.getMinutes() < 10)?'0'+time.getMinutes():time.getMinutes()}:${(time.getSeconds() < 10)?'0'+time.getSeconds():time.getSeconds()}">
+      ${(time.getHours() < 10)?'0'+time.getHours():time.getHours()}:${(time.getMinutes() < 10)?'0'+time.getMinutes():time.getMinutes()}:${(time.getSeconds() < 10)?'0'+time.getSeconds():time.getSeconds()}
+    </time >
+    `;
+  }
+}
--- dev/app/pages/user/user.js
+++ dev/app/pages/user/user.js
@@ -7,6 +7,7 @@
 */
 
 import  { UnsplashService } from '../../providers/unsplash/unsplash-service';
+import  { TimerComponent } from '../../components/timer/timer-component'
 
 export class UserPage {
 
@@ -60,28 +61,7 @@ export class UserPage {
   }
 
   displayTime(){
-    let timeElement = document.getElementById('time')
-    // some css with JS for time txt
-    timeElement.innerHTML = this.getTime(this.time)
-    timeElement.style.fontSize = '10rem';
-    timeElement.style.margin = '0rem';
-    timeElement.style.textShadow = '0px 0px 50px rgba(0, 0, 0, 0.21)';
-    // run interval
-    setInterval(()=>{
-      // asigne a new Date()
-      this.time = new Date();
-      //console.log(`${this.time.getHours()}:${this.time.getMinutes()}:${this.time.getSeconds()}`)
-      // replace innerHTML of time element
-      timeElement.innerHTML = this.getTime(this.time)
-    },1000)
-  }
-
-  getTime(time){
-    return    `
-    <time datetime="${(time.getFullYear() < 10)?'0'+time.getFullYear():time.getFullYear()}-${(time.getMonth() < 10)?'0'+time.getMonth():time.getMonth()}-${(time.getDate() < 10)?'0'+time.getDate():time.getDate()} ${(time.getHours() < 10)?'0'+time.getHours():time.getHours()}:${(time.getMinutes() < 10)?'0'+time.getMinutes():time.getMinutes()}:${(time.getSeconds() < 10)?'0'+time.getSeconds():time.getSeconds()}">
-      ${(time.getHours() < 10)?'0'+time.getHours():time.getHours()}:${(time.getMinutes() < 10)?'0'+time.getMinutes():time.getMinutes()}:${(time.getSeconds() < 10)?'0'+time.getSeconds():time.getSeconds()}
-    </time >
-    `;
+    new TimerComponent()
   }
 
   getBackgroundIMG(){
